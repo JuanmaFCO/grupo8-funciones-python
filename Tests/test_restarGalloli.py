db8e4df108dd9b5f98da7ca8a9d25a9b8f9040a2
EmaReBoUnD <emmanuelgalloli@gmail.com>
2025-11-04 21:42:39 -0300

    agrege funcion restar

--- Tests/test_restarGalloli.py
+++ Tests/test_restarGalloli.py
@@ -1,5 +1,7 @@
 #tests/test_restar.py
-from funciones.restar import restar
+
+from Funciones.restarGalloli import restar
+
 def test_restar():
-    assert restar(10, 4) == 6
-    assert restar(5, 10) == -5
+ assert restar(10, 4) == 6
+ assert restar(5, 10) == -5
