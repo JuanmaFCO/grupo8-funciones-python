6992b908edacc1447c95259849a4882b22cf627c
Pao Cabrera <90774809+paoCabreraCursos@users.noreply.github.com>
2025-11-06 12:32:11 -0300

    Delete Tests directory

--- Tests/test_dividirRodriguez.py
+++
@@ -1,6 +0,0 @@
-#Tests/test_dividirRodriguez.py
-from funciones.dividirRodriguez import dividir
-
-def test_dividir():
-	assert dividir(10, 2) == 5
-	assert dividir(5, 0) is None
--- Tests/test_modulo_fernandez.py
+++
@@ -1,7 +0,0 @@
-# test/test_modulo_fernandez.py
-
-from funciones.modulo_fernandez import modulo_fernandez
-
-def test_modulo_fernandez():
-    assert modulo_fernandez(10, 3) == 1
-    assert modulo_fernandez(7, 0) is None
